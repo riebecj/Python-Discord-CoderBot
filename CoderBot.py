5af24bc161b839eda8888147abb30be9195ebe68
riebecj <casey.riebe@yahoo.com>
2018-12-20 07:38:46 -0800

    Various changes
    - Added Leaderboard
    - Pushes traceback on Exception
    - Tracks Challenge time
    - Added comments and doc strings

--- CoderBot.py
+++ CoderBot.py
@@ -1,21 +1,13 @@
-import discord
-import base64
-import requests
-import pickle
-import cloudpickle as cp
-from urllib.request import urlopen
-import dynmodule  # name of this module
-import textwrap  # for more readable code formatting in sample string
+from datetime import datetime, timedelta
+import logging.config
+import dynmodule
+import textwrap  
 import dateparser
 import random
 import asyncio
-from datetime import datetime, timedelta
 import discord
-import json
-import logging
-import logging.config
-import re
-from signal import SIGINT, SIGTERM
+import operator
+import traceback
 
 
 log = logging.getLogger(__name__)
@@ -24,96 +16,103 @@ loop = asyncio.get_event_loop()
 
 class Bot(object):
     def __init__(self):
-        # Main parts of the bot
-        self.token = 'TOKEN'
+        # token for Discord API (VERY SECRET)
+        self.token = 'TOKEN_STRING'
+        # prefix for all commands to bot
         self.prefix = '!'
+        # specific channel lock. Won't reply or post to any other public channel
         self.channel_lock = 'proving-grounds'
+        # sets the client 
         self.client = discord.Client(loop=loop)
+        # dictionary for commands
         self.commands = dict()
+        # list of supported Languages
         self.supported_langs = None
+        # adds the event handler for messages
         self.client.event(self.on_message)
+        # get bot start time
         self._start_time = datetime.now()
+        # dict for holding leaderboard
+        self.alltime_leaders = {}
+        # dict for holding challenge scores
+        self.challenge_participants = {}
+        # database of pointers to language specific questions
         self.database = None
-        self.title = None
-        self.question = None
-        self.test_cases = None
-        self.answers = None
-        self.set_commands()
-        self.channels = None
+        # Bool to detect ongoing challenge
         self.ongoing = False
+        # Title of challenge problem
+        self.title = None
+        # Challenge problem
+        self.question = None
+        # list of test cases
+        self.test_cases = None
+        # list of answers for each test case
+        self.answers = None
+        # int of seconds left for the challenge (used in computing score)
         self.challenge_time = 0
+        # total time of challenge (int of seconds used in computing score)
+        self.total_time = 0
+        # Total points for the question 
         self.points = 0
-        self.challenge_participants = None
+        # current challenge language
+        self.lang = None
+        # set the bot commands
+        self.set_commands()
+        # get various pointers for each language supported
         self.set_database()
+        # read in the all time leaderboard 
+        self.get_leaderboard()
+
+    def get_leaderboard(self):
+        """Gets the leaderboard from the persistent file"""
+        self.alltime_leaders = eval(open(r'leader_data/leaderboard', 'rt').read())
+
+    def write_leaderboard(self):
+        """Overwrites the leaderboard file with new values"""
+        with open(r'leader_data/leaderboard', 'wt') as lb:
+            lb.truncate()
+            lb.write(str(self.alltime_leaders))
+
+    def update_alltime(self):
+        """Update the All Time Leaderboard (usually after a challange is complete."""
+        for user, score in self.challenge_participants.items():
+            if user in self.alltime_leaders.keys():
+                self.alltime_leaders[user] += score
+            else:
+                self.alltime_leaders[user] = score
+        self.write_leaderboard()
 
     def set_commands(self):
+        """Set the bot command to the function"""
         self.commands = {
             '!help': self._help,
             '!stats': self._stats,
             '!info': self._info,
             '!hello': self._greeting,
-            '!schedule': self._schedule_event
+            '!schedule': self._schedule_event,
+            '!leaderboard': self._post_alltime
         }
 
-    def test_submission(self, user, submission):
-        for test_case, answer in zip(self.test_cases, self.answers):
-            if self.run(submission, test_case) == answer:
-                continue
-            else:
-                return False
-        return True
-
-    def set_database(self):
-        lang = 'python'
-        self.database = {
-            'python': 'python-data/questions'
-        }
-        self.supported_langs = self.database.keys()
-
-    def run(self, func, *args):
-        module_code = textwrap.dedent(func)
-        dynmodule.load(module_code)  # defines module's contents
-        return dynmodule.func(*args)
-
-    async def end_challenge(self, channel):
-        self.ongoing = False
-        msg = 'That is the end of this Coding Challenge!\n Congratulations to {0.display_name} for ' \
-              'scoring the most points!'.format()
-        await self.client.send_message(channel, msg)
-
-    async def begin_challenge(self, lang, channel):
-        self.ongoing = True
-        available = eval(open(self.database[lang.lower()], 'rt').read())
-        selection = available[random.randint(0, len(available.keys()) - 1)]
-        self.title = selection['title']
-        self.question = selection['question']
-        self.test_cases = selection['test_cases']
-        self.answers = selection['answers']
-        self.challenge_time = selection['time_limit']
-        msg = "Welcome to the {} coding challenge!".format(lang)
-        msg2 = 'A few rules before we begin...'
-        msg3 = '1. All submission will be preceded by a `$`. This ensures I can parse it properly.'
-        msg4 = "2. A correct answer will not end the contest, but award you points based upon how fast you submit " \
-               "a correct answer."
-        msg5 = "3. An incorrect answer will result in the wrong or invalid output (possibly traceback) being DM'ed " \
-               "to you so you can work to correct your code."
-        msg6 = "4. The initial part of the requested function will be given to you at the start of the challenge. " \
-               "(e.g. `$def find_least_mean_square(listA, listB):`). This is to ensure proper execution and " \
-               "validation on my end"
-        msg7 = "You may either code in the Discord chat box (Hold `Shift` when pressing enter for a new line) or use " \
-               "a text editor or your favorite IDE and copy/paste the code into the chat box."
-        msg8 = "Now, without further ado, we begin in 5..."
-
-        for msg in [msg, msg2, msg3, msg4, msg5, msg6, msg7, msg8]:
-            await self.client.send_message(channel, msg)
-            await asyncio.sleep(5)
-
-        for i in range(4):
-            await asyncio.sleep(1)
-            await self.client.send_message(channel, "{}...".format(4-i))
-        await self.client.send_message(channel, "\n{}\n\n{}".format(self.title, self.question))
-        await self.challenge_timer(channel, self.challenge_time)
-
+    async def test_submission(self, message):
+        """Test the user's code submission"""
+        user = message.author
+        submission = message.content[1:]
+        for number, (test_case, answer) in enumerate(zip(self.test_cases, self.answers)):
+            try:
+                output = self.run(submission, test_case)
+                if output == answer:  # passed test case
+                    continue
+                else:  # failed test case
+                    if number > 2:  # only show the first 3 test cases. The rest are hidden.
+                        test_case = 'hidden'
+                        answer = 'hidden'
+                    await self.failed(user, test_case, output, answer)  
+                    return
+            except:  # bad syntax 
+                msg = traceback.format_exc()
+                await self.client.send_message(user, msg)
+                return
+        await self.correct(user)
 
     def round_time(self, dt=None, round_to=60*60):
         """Round a datetime object to any time lapse in seconds
@@ -127,7 +126,78 @@ class Bot(object):
         rounding = (seconds + round_to / 2) // round_to * round_to
         return dt + timedelta(0, rounding - seconds, -dt.microsecond)
 
+    def set_database(self):
+        """Ger pointers to various supported language questions"""
+        self.database = {
+            'python': 'python-data/questions'
+        }
+        self.supported_langs = self.database.keys()  # sets list of supported languages
+
+    def run(self, func, *args):
+        """executes users code in specific environments for safety"""
+        if self.lang.lower() == 'python':
+            module_code = textwrap.dedent(func)
+            dynmodule.load(module_code)  # defines module's contents
+            return dynmodule.func(*args)
+
+    def award_points(self, user):
+        """Calculate user's score upon correct submission"""
+        points = round((self.challenge_time / self.total_time) * self.points)
+        self.challenge_participants[user] = points
+
+    async def end_challenge(self, channel):
+        """Handle ending the Challenge event"""
+        self.ongoing = False
+        user, points = max(self.challenge_participants.items(), key=operator.itemgetter(1))
+        msg = '\nThat is the end of this Coding Challenge!\n Congratulations to {0.display_name} for ' \
+              'scoring the most points with {1}!\n'.format(user, points)
+        await self.client.send_message(channel, msg)
+
+        msg = '\nResults\n----------\n'
+        for user, points in reversed(sorted(self.challenge_participants.items(), key=lambda kv: kv[1])):
+            msg += "`{0.display_name}` : `{1}` points\n".format(user, points)
+        await self.client.send_message(channel, msg)
+        self.update_alltime()
+        self.write_leaderboard()
+        self.challenge_participants = {}
+
+    async def begin_challenge(self, lang, channel):
+        """"Handle the beginning of the challenge event"""
+        self.ongoing = True
+        available = eval(open(self.database[lang.lower()], 'rt').read())
+        selection = available[random.randint(0, len(available.keys()) - 1)]
+        self.title = selection['title']
+        self.question = selection['question']
+        self.test_cases = selection['test_cases']
+        self.answers = selection['answers']
+        self.challenge_time = self.total_time = selection['time_limit']*60
+        self.points = selection['total_points']
+        msg = "Welcome to the {} coding challenge!".format(lang)
+        msg2 = 'A few rules before we begin...'
+        msg3 = '1. All submission will be preceded by a `$`. This ensures I can parse it properly.'
+        msg4 = "2. A correct answer will not end the contest, but award you points based upon how fast you submit " \
+               "a correct answer."
+        msg5 = "3. An incorrect answer will result in the wrong or invalid output (possibly traceback) being DM'ed " \
+               "to you so you can work to correct your code."
+        msg6 = "4. The initial part of the requested function will be given to you at the start of the challenge. " \
+               "(e.g. `$def find_least_mean_square(listA, listB):`). This is to ensure proper execution and " \
+               "validation on my end"
+        msg7 = "5. You may either code in the Discord chat box (Hold `Shift` when pressing enter for a new " \
+               "line) or use a text editor or your favorite IDE and copy/paste the code into the chat box."
+        msg8 = "Now, without further ado, we begin in 5..."
+
+        for msg in [msg, msg2, msg3, msg4, msg5, msg6, msg7, msg8]:
+            await self.client.send_message(channel, msg)
+            await asyncio.sleep(5)
+
+        for i in range(4):
+            await asyncio.sleep(1)
+            await self.client.send_message(channel, "{}...".format(4-i))
+        await self.client.send_message(channel, "\n{}\n{}".format(self.title, self.question))
+        await self.challenge_timer(channel, self.challenge_time)
+
     async def start(self):
+        """Login"""
         await self.client.login(self.token)
 
         try:
@@ -136,42 +206,50 @@ class Bot(object):
             raise
 
     async def stop(self):
+        """Exit"""
         await self.client.logout()
 
+    async def correct(self, user):
+        """Executes when user submits a correct solution"""
+        self.award_points(user)
+        msg = "You passed all test cases!"
+        await self.client.send_message(user, msg)
+
+    async def failed(self, user, _input, output, expected):
+        """Executes when the user failed a test case"""
+        msg = "I'm sorry. You're submission did not pass all test cases.\n" \
+              "Input: `{}`\nExpected output: `{}`\nYour output: `{}`".format(_input, expected, output)
+        await self.client.send_message(user, msg)
+
     async def on_message(self, message):
-        if 'BotModerator' not in [role.name for role in message.author.roles]:
+        """Handle messages as commands"""
+        if message.author == self.client.user:  # do not respond to itself
+            return
+        
+        # do not respond to any other channel other than the designated one
+        if message.channel.name != self.channel_lock:  
             return
 
-        if message.channel.is_private:
-            msg = 'Busy. Please stand by...'
-            await self.client.send_message(message.channel, msg)
-            return
-
-        # we do not want the bot to reply to itself
-        if message.author == self.client.user:
-            return
-
-        if message.channel.name != self.channel_lock:
-            return
-
+        # handle code submissions during challanges
         if message.content.startswith('$'):
             if self.ongoing:
                 await self.client.delete_message(message)
                 msg = 'Thank you for your submission, {0.author.display_name}.'.format(message)
-                self.test_submission(message.author, message.content)
+                if message.author not in self.challenge_participants.keys():
+                    await self.test_submission(message)
+                else:
+                    await self.client.send_message(message.author, 'You have already submitted a correct '
+                                                                   'solution and cannot submit again.')
             else:
-                msg = "I'm sorry, {0.author.display_name}, " \
-                      "but there is no active coding challenge at the moment.".format(message)
-                await self.client.send_message(message.channel, msg)
                 return
 
+        # handle standard commands
         if message.content.startswith(self.prefix):
             for command, func in self.commands.items():
                 if command in message.content:
                     await func(message)
                     return
 
-    # Commands
     async def _help(self, message):
         """Print the help message"""
         msg = 'Available commands:\n'
@@ -196,17 +274,20 @@ class Bot(object):
                                                "`{}` is not a known time constraint.".format(sked_time))
                 return
             else:
-                lang = msg_args[1]
-                if lang.lower() not in self.supported_langs:
+                self.lang = msg_args[1]
+                if self.lang.lower() not in self.supported_langs:
                     await self.client.send_message(message.channel,
-                                                   "`{}` is not a supported language.".format(lang))
+                                                   "`{}` is not a supported language.".format(self.lang))
                     return
             if self.round_time(sked_time, 60) == self.round_time(datetime.now(), 60):
-                await self.begin_challenge(lang, message.channel)
+                await self.begin_challenge(self.lang, message.channel)
                 return
-        self.client.loop.create_task(self.schedule_tracker(message, self.round_time(sked_time), lang))
+        self.client.loop.create_task(self.schedule_tracker(message, self.round_time(sked_time), self.lang))
 
     async def schedule_tracker(self, message, event_time, lang):
+        """background task to keep track of scheduled event and send reminders"""
+        if 'BotModerator' not in [role.name for role in message.author.roles]:
+            return
         idioms = ['Be there or be square!', 'Show up or shut up (respectfully)!', "Don't miss the boat!",
                   "You can't win if you don't play!", 'You snooze, you lose!']
         await self.client.wait_until_ready()
@@ -214,7 +295,6 @@ class Bot(object):
         idiom = idioms[random.randint(0, len(idioms)-1)]
         msg = 'A `{}` coding challenge has been scheduled for {} PST.\n{}'.format(lang, event_time, idiom)
         await self.client.send_message(channel, msg)
-        note1 = note2 = note3 = False
         while datetime.now() < event_time:
             days = (event_time - datetime.now()).days
             hours = (event_time - datetime.now()).hours
@@ -231,11 +311,12 @@ class Bot(object):
         await self.begin_challenge(lang, channel)
 
     async def challenge_timer(self, channel, challenge_time):
-        event_time = datetime.now() + timedelta(minutes=challenge_time)
+        """Timer for Challenge events"""
+        event_time = datetime.now() + timedelta(seconds=challenge_time)
         while datetime.now() < event_time:
-            if self.challenge_time % 5 == 0:
+            if self.challenge_time % 300 == 0:
                 await self.client.send_message(channel, "{} remaining in this challenge".format(event_time-datetime.now()))
-            await asyncio.sleep(60)
+            await asyncio.sleep(1)
             self.challenge_time -= 1
         await self.end_challenge(channel)
 
@@ -257,6 +338,13 @@ class Bot(object):
     async def _greeting(self, message):
         """Say 'Hi' to the bot"""
         msg = 'Hello, {0.author.display_name}!'.format(message)
+        await self.client.send_message(message.channel, msg)
+
+    async def _post_alltime(self, message):
+        """Display All Time Leader Board."""
+        msg = 'ALL TIME LEADER BOARD\n----------------------\n'
+        for user, score in reversed(sorted(self.alltime_leaders.items(), key=lambda kv: kv[1])):
+            msg += "{0.display_name} : {1} Points".format(user, score)
         await self.client.send_message(message.channel, msg)
 
 
